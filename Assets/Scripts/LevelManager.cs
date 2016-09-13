28312d0e20a7a16eb1519bddb1ff3c3af76355ee
Kevin de Ridder <kridder@hotmail.com>
2016-09-13 01:45:54 -0700

    Initialized git, created spalsh scene, menu scene and main scene.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager instance = null;
+    public float autoLoadNextLevelAfter;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+	public void LoadLevel(string name)
+    {
+        Debug.Log("New Level load: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// adds class boundary to set the bounds in which the player can move around
+// data members can be set in Unity editor
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private new PolygonCollider2D collider;
+
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<PolygonCollider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Player Movement 
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb.velocity = movement * speed;
+
+        //defines range of players movement
+        rb.position = new Vector2
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax)
+        );
+
+        //ship rotation
+        rb.transform.rotation = Quaternion.Euler(0.0f, rb.velocity.x * -tilt, 0.0f);
+    }
+}
