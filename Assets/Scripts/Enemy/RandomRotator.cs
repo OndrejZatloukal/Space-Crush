cfba92e9b9282c73cecbf305d499795249f71776
Kevin de Ridder <kridder@hotmail.com>
2016-09-18 19:46:42 -0700

    Added random rotator to mines and updated sprites.

---
+++ Assets/Scripts/Enemy/RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.angularVelocity = Random.Range(-1.0f, 1.0f) * rotationSpeed;
+    }
+}
