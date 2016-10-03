476ef0653ec294904feaa817e81b4a00aa379c24
Kevin de Ridder <kridder@hotmail.com>
2016-10-03 00:24:50 -0700

    Updated options menu with new art.

--- Assets/Scripts/Managers/OptionButtonsManager.cs
+++ Assets/Scripts/Managers/OptionButtonsManager.cs
@@ -5,12 +5,19 @@ using UnityEngine.UI;
 public class OptionButtonsManager : MonoBehaviour
 {
     public GameObject volumeSlider;
+    public Image volumeOff;
+    public Sprite volumeOffOff;
+    public Sprite volumeOffOn;
+    public Image volumeOn;
+    public Sprite volumeOnOn;
+    public Sprite volumeOnOff;
     public GameObject customCursor;
     public GameObject cursorMode;
 
     void Start()
     {
         volumeSlider.GetComponent<Slider>().value = SoundManager.instance.masterVolume;
+        SetVolumeIcons();
 
         if (CursorManager.instance.customCursors)
         {
@@ -34,6 +41,21 @@ public class OptionButtonsManager : MonoBehaviour
     public void SetMasterVolume()
     {
         SoundManager.instance.SetMasterVolume(volumeSlider.GetComponent<Slider>().value);
+        SetVolumeIcons();
+    }
+
+    private void SetVolumeIcons()
+    {
+        if (SoundManager.instance.masterVolume == 0)
+        {
+            volumeOff.sprite = volumeOffOn;
+            volumeOn.sprite = volumeOnOff;
+        }
+        else
+        {
+            volumeOff.sprite = volumeOffOff;
+            volumeOn.sprite = volumeOnOn;
+        }
     }
 
     public void ToggleCustomCursor()
